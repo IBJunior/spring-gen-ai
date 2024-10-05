df24ec60c7f76ead833c2d9de20eb48d2850c889
Ali ibrahim <iali96369@gmail.com>
2024-10-05 19:40:16 +0200

    Added Security

---
+++ src/main/java/fly/intelligent/genai/configurations/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package fly.intelligent.genai.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/conversation-history",
+                                "/api/conversation-history/**", "/api/assistant", "/api/assistant/**")
+                        .hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/actuator/health", "/actuator/info")
+                        .permitAll()
+                        .requestMatchers("/actuator", "/actuator/**")
+                        .hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
